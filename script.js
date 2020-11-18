e2e949fcfabf9de0812925c73a20a197c55a7fb0
Tierra <tierrarucker@ymail.com>
2020-11-17 19:59:16 -0500

    update files after github repository name change

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-// Project: When the button it pressed it will display the day or month or season
+// Project: When the button is pressed it will display the day or month or season
 // Step 1: create variables to store info: DONE
 // Step 2: create the funtion to get day: DONE
 // Step 3: create the funtion to get month: DONE
-// CURRENTLY >> Step 4: create the funtion to get season: in progress
-// Step 5: testing
-// Project: in progress
+// CURRENTLY >> Step 4: create the funtion to get season: DONE
+// Step 5: testing: DONE
+// Project: DONE
 
 
 // Step 1: create variables to store info
@@ -16,12 +16,7 @@ const months = ["January", "February", "March", "April", "May", "June", "Julie",
 
 let currentSeason; // Spring: March 1-May 31; Summer June 1-August 31; Autumn September 1; Winter December 1-February 29.
 // Winter: December-February; Spring: March-May; Summer: June-August; Autumn: September - November
-const seasons = [
-  "Spring", 
-  "Summer", 
-  "Autumn", 
-  "Winter"
-];
+
 
 // Step 2: create the funtion to get day
 function getDay(){
@@ -42,10 +37,22 @@ function getMonth(){
 // Step 4: create the funtion to get season
 function getSeason(){
   var d = new Date();
-  currentSeason = 
+  currentSeason = months[d.getMonth()];
+  if (currentSeason == "December" || currentSeason == "January" || currentSeason == "February"){
+    document.getElementById("currentSeason").innerHTML = "Winter";
+    return;
+  } else if (currentSeason == "March" || currentSeason == "April" || currentSeason == "May"){
+    document.getElementById("currentSeason").innerHTML = "Spring";
+    return;
+  } else if (currentSeason == "June" || currentSeason == "July" || currentSeason == "August"){
+    document.getElementById("currentSeason").innerHTML = "Summer";
+    return;
+  } else if (currentSeason == "September" || currentSeason == "October" || currentSeason == "November"){
+    document.getElementById("currentSeason").innerHTML = "Autumn";
+    return;
+  };
   console.log("The season is " + currentSeason + ".");
-  // document.getElementById("currentSeason").innerHTML = currentSeason;
 }
 
-// Step 5: test project
-// Project: in progress
+// Step 5: test project > COMPLETE
+// Project: COMPLETE
